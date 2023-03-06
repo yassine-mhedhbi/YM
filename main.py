fe46f2c7e04aa30a0d7b69467de5c073ffcf97a9
Yassine Mhedhbi <yassine.mhed@gmail.com>
2023-03-05 20:51:57 -0500

    nit

--- main.py
+++ main.py
@@ -29,13 +29,14 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    HTTPSRedirectMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
 
 )
+
+app.add_middleware(HTTPSRedirectMiddleware)
 
 
 @app.exception_handler(RequestValidationError)
